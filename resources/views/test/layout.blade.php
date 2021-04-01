98f9c184753a5da75bc1e20de0a46daea9bb25aa
Tanzila Mustaqim <tanzilal14898@gmail.com>
2021-04-01 11:24:39 +0700

    add navbar

--- resources/views/test/layout.blade.php
+++ resources/views/test/layout.blade.php
@@ -27,6 +27,25 @@
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="{{route('barang.index')}}">Barang</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="{{route('detail-penjualan.index')}}">Detail Penjualan</a>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+      </nav>
     @yield('IsiHalaman')
 
     <!-- Optional JavaScript; choose one of the two! -->
