168822a9bd22b70f450220d6ea6790641d51443a
Endar <cuti@gmail.com>
2021-04-01 11:10:54 +0700

    add penjualan

---
+++ resources/views/user/penjualan.blade.php
@@ -0,0 +1,9 @@
+@extends('test.layout')
+@section('JudulHalaman', 'Toko Mas Woko')
+@section('content')
+    <div class="container">
+        <center>
+            <h2>Penjualan</h2>
+        </center>
+    </div>
+@endsection
