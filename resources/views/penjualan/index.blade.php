892bdd226348899fe774081eea7a7e55c5e97b3f
Endar <cuti@gmail.com>
2021-04-03 21:30:36 +0700

    April 3, 2021 9:30 PM

--- resources/views/penjualan/index.blade.php
+++ resources/views/penjualan/index.blade.php
@@ -5,12 +5,11 @@
 @section('content')
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-12">
-                <div class="dashtwo-order-list shadow-reset">
-                    <div class="row">
-
-                    </div>
-                </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                asdas
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                asdas
             </div>
         </div>
     </div>
