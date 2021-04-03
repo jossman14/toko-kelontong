bd920b3206ce3bd4cb39a1a074db3cec01005484
Endar <cuti@gmail.com>
2021-04-03 20:49:11 +0700

    April 3, 2021 8:49 PM

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -73,7 +73,7 @@
                         <div class="income-dashone-pro">
                             <div class="income-rate-total">
                                 <div class="price-adminpro-rate">
-                                    <h3><span class="counter">4400</span></h3>
+                                    <h3><span class="counter">{{ $barang }}</span></h3>
                                 </div>
                                 <div class="price-graph">
                                     <span id="sparkline2"></span>
