3652d1f3c27eebb2d03976fd8aceba9cf386619f
Endar <cuti@gmail.com>
2021-04-05 09:15:19 +0700

    April 5, 2021 9:15 AM

--- resources/views/penjualan/index.blade.php
+++ resources/views/penjualan/index.blade.php
@@ -6,6 +6,13 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-6">
+                <div class="">
+                    <h4>Transaksi Kasir</h4>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
                 <div class="dashtwo-order-list">
                     sdsd
                 </div>
