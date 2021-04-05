63c29a9ba73d45d156a2e9e54ef02421ab7827af
Endar <cuti@gmail.com>
2021-04-05 09:53:18 +0700

    April 5, 2021 9:53 AM

--- resources/views/penjualan/index.blade.php
+++ resources/views/penjualan/index.blade.php
@@ -3,22 +3,19 @@
 @section('breadcome', 'Transaksi')
 
 @section('content')
-    <div class="container-fluid">
-        <div class="judul-transaksi">
-
-            <h4>Transaksi Penjualan</h4>
-
-        </div>
-        <div class="content-transaksi">
+    <div class="data-table-area mg-b-15">
+        <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-6">
-                    <div class="dashtwo-order-list">
-                        sdsd
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="dashtwo-order-list">
-                        sdsd
+                <div class="col-lg-12">
+                    <div class="sparkline13-list shadow-reset">
+                        <div class="sparkline13-hd">
+                            <div class="main-sparkline13-hd">
+                                <h1>Transaksi Penjualan</h1>
+                                <div class="sparkline13-outline-icon">
+                                    <span class="sparkline13-collapse-link"><i class="fa fa-chevron-up"></i></span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
