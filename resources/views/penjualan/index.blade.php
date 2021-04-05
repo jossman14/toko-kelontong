2a7ec654309d1c3ec299ae38e7ff29670b89bae9
Endar <cuti@gmail.com>
2021-04-05 09:43:01 +0700

    April 5, 2021 9:43 AM

--- resources/views/penjualan/index.blade.php
+++ resources/views/penjualan/index.blade.php
@@ -4,22 +4,22 @@
 
 @section('content')
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-12 judul-transaksi">
-                <div class="dashtwo-order-list">
-                    <h4>Transaksi Penjualan</h4>
-                </div>
-            </div>
+        <div class="judul-transaksi">
+
+            <h4>Transaksi Penjualan</h4>
+
         </div>
-        <div class="row">
-            <div class="col-lg-6">
-                <div class="dashtwo-order-list">
-                    sdsd
+        <div class="content-transaksi">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="dashtwo-order-list">
+                        sdsd
+                    </div>
                 </div>
-            </div>
-            <div class="col-lg-6">
-                <div class="dashtwo-order-list">
-                    sdsd
+                <div class="col-lg-6">
+                    <div class="dashtwo-order-list">
+                        sdsd
+                    </div>
                 </div>
             </div>
         </div>
