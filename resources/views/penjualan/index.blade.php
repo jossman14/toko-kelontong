da2a28dc88d78cac0b3b6d20c0a2c86f01007703
Endar <cuti@gmail.com>
2021-04-05 09:19:03 +0700

    April 5, 2021 9:19 AM

--- resources/views/penjualan/index.blade.php
+++ resources/views/penjualan/index.blade.php
@@ -5,10 +5,8 @@
 @section('content')
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-6">
-                <div class="">
-                    <h4>Transaksi Kasir</h4>
-                </div>
+            <div class="col-lg-12 judul-transaksi">
+                <h4>Transaksi Kasir</h4>
             </div>
         </div>
         <div class="row">
